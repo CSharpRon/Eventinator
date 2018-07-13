080825598a90f516442fb37b363697ba7aed8752
sujeom <martinez344j@gmail.com>
2018-07-13 17:17:15 -0400

    Changing marker position by clicking

--- src/Components/googleMaps/Markers.js
+++ src/Components/googleMaps/Markers.js
@@ -4,6 +4,9 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 import {KEY} from "./account"
 
 
+
+
+
 const MyMapComponent = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key="+KEY+"&libraries=geometry,drawing,places",
@@ -16,27 +19,51 @@ const MyMapComponent = compose(
             const refs = {}
 
             this.setState({
-                position: null,
+                position: { lat: 28.6024, lng: -81.2001 },
                 onMarkerMounted: ref => {
                     refs.marker = ref;
                 },
 
                 onPositionChanged: () => {
-                    const position = refs.marker.getPosition();
+                    const position = refs.marker.getPosition;
                     console.log(position.toString());
-                }
+                },
+
+                onClick: (info) => {
+                    let info1 = info
+                    console.log(info.la)
+                    console.log(info.latLng)
+                    console.log(info)
+                    this.setState({ position: info.latLng })
+                    const position = info.latLng;                    
+                    return(<Marker position={{ lat: info.lat, lng: info.lng}}/>)
+                },
+
+                
+
+
+                isMarkerShown: true,
             })
         },
     }),
     withScriptjs,
     withGoogleMap
 )((props) =>
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} draggable={true} ref={props.onMarkerMounted} onPositionChanged={props.onPositionChanged} />}
+    <GoogleMap defaultZoom={15} defaultCenter={{ lat: 28.6024, lng: -81.2001 }} onClick={props.onClick}>
+        {props.isMarkerShown && <Marker position={props.position} draggable={true} ref={props.onMarkerMounted} onPositionChanged={props.onPositionChanged} onClick = {props.onClick}/>}
     </GoogleMap>
     )
 
 class Markers extends React.PureComponent {
+    
+    constructor(){
+        super();
+        this.state = {
+            isMarkerShown: false,
+            position: { lat: -34.397, lng: 150.644 }
+        }
+
+    }
     state = {
         isMarkerShown: false,
     }
@@ -44,9 +71,13 @@ class Markers extends React.PureComponent {
 
     render() {
         return (
+        <div>
             <div>
                 <MyMapComponent isMarkerShown={true} />
             </div>
+
+        {/*<button title="Log" onClick={()=> console.log(this.props.onPositionChanged}> Penis </button> */}
+        </div>
         )
     }
 }
