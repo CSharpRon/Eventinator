af60dadbf4deb304d457267c7dee89cfc3f20b12
Camilo Lozano <camilolozano@Camilos-MacBook-Pro.local>
2018-07-16 12:30:05 -0400

    Fixed side panek

--- src/Components/events/EventSidePanel.js
+++ src/Components/events/EventSidePanel.js
@@ -67,57 +67,53 @@ export class EventSidePanel extends Component {
 
                 } }>
                     <div class="modal-body">
-                        <form id="add_contact_form" action="{{ url_for('add_contact') }}" method="post">
-                                <div class="form-group">
-                            <div class="form-group">
-                                <div class="form-group col-md-6">
-                                    <label for="inputFirstName">Event Name</label>
-                                    <input name="event_name" type="name" class="form-control" id="eventName" ref="eventName" placeholder="Event Name" required="">
-                                    </input>
-                                </div>
-                                
-                                <div class="form-group">
-                                    <div class="form-group col-md-6">
-                                        <label for="description">Description</label>
-                                        <input name="description" type="name" class="form-control" id="description" placeholder="Description" ref="description" required="">
-                                        </input>
-                                    </div>
-                                
-                                    <div class="form-group">
-                                        <label for="date">Date</label>
-                                        <input name= "date" type="text" class="form-control" id="date" ref="date" placeholder="12/27/2018">
-                                        </input>
-                                        <div class="form-group">
-                                            <label for="phone">Phone Number</label>
-                                            <input name="phone" type="text" class="form-control" id="phone" ref="phone" placeholder="555 555-5555">
-                                            </input>
-                                            <div class="form-group">
-                                                <label for="email">Email</label>
-                                                <input name="email" type="text" class="form-control" id="email" ref ="email" placeholder="titties@titties.edu">
-                                                </input>
-                                                <div class="form-row">
-                                                    <div class="form-group col-md-9">
-                                                        <label for="category">Category</label>
-                                                        <input name="category" type="text" class="form-control" id="category" ref="category">
-                                                        </input>
-                                                        <div class="form-group col-md-10">
-                                                            <label for="attendees">Attendees</label>
-                                                            <input name="attendees" type="text" class="form-control" id="attendees" ref="attendees">
-                                                            </input>
-                                                        </div>
-                                                        <button id="add_contact_submit" type="button" data-dismiss="modal" class="btn btn-primary" onClick={() => this.addEvent(this.props)} >Add Contact</button>
-                                                        <button type="button" class="btn btn-alert" data-dismiss="modal">Cancel</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <br />
+  
+  <div class="form-group">
+     <label for="inputFirstName">Event Name</label>
+     <input name="event_name" type="name" class="form-control" id="eventName" ref="eventName" placeholder="Event Name" required="">
+     </input>
+   </div>
+   
+   <div class="form-group">
+       <label for="description">Description</label>
+       <input name="description" type="name" class="form-control" id="description" placeholder="Description" ref="description" required="">
+       </input>
+   </div>
+   
+   <div class="form-group">
+     <label for="date">Date</label>
+     <input name= "date" type="text" class="form-control" id="date" ref="date" placeholder="12/27/2018">
+     </input>
+   </div>
+   
+   <div class="form-group">
+     <label for="phone">Phone Number</label>
+     <input name="phone" type="text" class="form-control" id="phone" ref="phone" placeholder="555 555-5555">
+     </input>
+   </div>
+   
+   <div class="form-group">
+     <label for="email">Email</label>
+     <input name="email" type="text" class="form-control" id="email" ref ="email" placeholder="titties@titties.edu">
+     </input>
+   </div>
+   
+   <div class="form-group">
+     <label for="category">Category</label>
+     <input name="category" type="text" class="form-control" id="category" ref="category">
+     </input>
+   </div>
+   
+   <div class="form-group">
+    <label for="attendees">Attendees</label>
+     <input name="attendees" type="text" class="form-control" id="attendees" ref="attendees">
+     </input>
+   </div>
+ 
+ <button id="add_contact_submit" type="button" data-dismiss="modal" class="btn btn-primary" onClick={() => this.addEvent(this.props)} >Add Contact</button>
+ <button type="button" class="btn btn-alert" data-dismiss="modal">Cancel</button>
+ 
+</div>
             </SlidingPane>
         </div>;
     }
